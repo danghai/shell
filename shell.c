71a0f1af217f7000754e2b128c45392f31d6f093
danghai <goldsea5191@gmail.com>
2017-07-24 12:02:58 -0700

    finish the parse command line

--- shell.c
+++ shell.c
@@ -16,16 +16,21 @@
 #include <sys/types.h>
 
 #define COMMAND_BUFSIZE 1024
+#define STRTOK_BUFSIZE 64
+#define STRTOK_DELIM " \t\r\n\a"
+
 char *lsh_read_command(void);
+char **lsh_parse_command(char *);
 
 int main(int argc, char *argv[])
 {
-  char * cmd;
+  char * cmd, **tokens;
   do {
     printf("MY SHELL > ");
     fflush (stdout);
     cmd = lsh_read_command();
-    printf("%s \n",cmd);  // for testing
+    tokens = lsh_parse_command(cmd);
+    printf("%s \n",tokens[1]);  // for testing
   } while(1);
 
   exit(EXIT_SUCCESS);
@@ -42,7 +47,7 @@ char *lsh_read_command()
   char * cmd = malloc (sizeof(char) *COMMAND_BUFSIZE);
   if(!cmd)
   {
-      perror("Error memory allocation");
+      perror("Error memory allocation -cmd");
       exit(EXIT_FAILURE);
   }
 
@@ -58,4 +63,29 @@ char *lsh_read_command()
       cmd[position] = '\0';
       return cmd;
   }
+}
+
+/*
+  lsh_parse_command: Function parse the command into a list of arguments
+*/
+
+char **lsh_parse_command(char *cmd)
+{
+  int position = 0;
+  char **tokens = malloc (sizeof(char *) * STRTOK_BUFSIZE);
+  char *token;
+  if(!tokens)
+  {
+      perror("Error memory allocation - tokens");
+      exit(EXIT_FAILURE);
+  }
+  token = strtok(cmd, STRTOK_DELIM);        // get the first word
+  while(token != NULL)
+  {
+      tokens[position] = token;             // Store parse string in tokens following position
+      position ++;                          // Update new position
+      token = strtok(NULL, STRTOK_DELIM );  // NULL must be used to get tokens from the previous string
+  }
+  tokens[position] = NULL;
+  return tokens;
 }
